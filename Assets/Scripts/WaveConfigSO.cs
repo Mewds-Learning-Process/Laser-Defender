ea20c988004e718e19ec0c0f23dfbb504f443c18
Mohamed ElHariri <mohamed-elhariri@hotmail.com>
2023-11-12 20:07:13 +0200

    Fixed a bug in the WaveConfig file

--- Assets/Scripts/WaveConfigSO.cs
+++ Assets/Scripts/WaveConfigSO.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = = "Wave Config", fileName = "New Wave Config")]
+[CreateAssetMenu(menuName =  "Wave Config", fileName = "New Wave Config")]
 public class WaveConfigSO : ScriptableObject
 {
     [SerializeField] Transform pathPrefab;
@@ -15,7 +15,7 @@ public class WaveConfigSO : ScriptableObject
 
     public List<Transform> GetWaypoints()
     {
-        List<Transform> waypoints = new List<Transform>;
+        List<Transform> waypoints = new List<Transform>();
         foreach(Transform child in pathPrefab)
         {
             waypoints.Add(child);
